db9bb225483b65366c1ecee5e953c1ec1441a3ca
Nawabsaad96 <nawabsaad96@gmail.com>
2026-01-28 09:03:09 +0530

    output

---
+++ exp-30.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <sys/stat.h>
+
+int main() {
+    struct stat st;
+    stat("sample.txt", &st);
+
+    printf("Permissions: ");
+    printf((st.st_mode & S_IRUSR) ? "r" : "-");
+    printf((st.st_mode & S_IWUSR) ? "w" : "-");
+    printf((st.st_mode & S_IXUSR) ? "x" : "-");
+    printf("\n");
+
+    return 0;
+}
